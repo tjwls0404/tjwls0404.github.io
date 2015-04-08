a25e84446d2122dfa327701cd456a583bd22e4a5
tjwls0404 <tjwls0404@hotmail.com>
2015-04-08 11:00:39 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+//After line animation
+setTimeout(function(){
+    $('#overlay-container').animate({top: "757px"}, 700)
+}, 900);
+
+$('#body a').each(function(){
+    var id = $(this).attr('id');
+    var $line = $('#body ul').find('.catline.active.'+id);
+
+   $(this).hover(function(){
+       $line.css('width','0px');
+       $line.animate({width:"100%"}, 300, function(){
+           if ( !$('#body a:eq(0)').filter(function() { return $(this).is(":hover"); }).length)  $line.css('width', '0px');
+       })
+       }, function() {
+           $line.css('width', '0px');
+       });
+});
